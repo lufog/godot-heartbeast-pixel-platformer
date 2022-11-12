daffae8d02a6dc7e29124de173bc3001585b0086
Алексей Смирнов <debugger94@gmail.com>
2022-11-12 21:09:39 +0300

    Hitboxes + Custom Resources

--- player/player.gd
+++ player/player.gd
@@ -1,11 +1,7 @@
 class_name Player extends CharacterBody2D
 
 
-@export var jump_velocity := -260.0
-@export var half_jump_velocity := -110
-@export var max_speed := 50.0
-@export var acceleration := 10.0
-@export var friction := 10.0
+@export var move_data: PlayerMovementData
 
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -26,17 +22,17 @@ func _physics_process(delta: float) -> void:
 	# Handle Jump.
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
-			velocity.y = jump_velocity
+			velocity.y = move_data.jump_velocity
 	else:
-		if Input.is_action_just_released("ui_up") and velocity.y < half_jump_velocity:
-			velocity.y = half_jump_velocity
+		if Input.is_action_just_released("ui_up") and velocity.y < move_data.half_jump_velocity:
+			velocity.y = move_data.half_jump_velocity
 	
 	
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = move_toward(velocity.x, direction * max_speed, acceleration) 
+		velocity.x = move_toward(velocity.x, direction * move_data.max_speed, move_data.acceleration) 
 	else:
-		velocity.x = move_toward(velocity.x, 0, friction)
+		velocity.x = move_toward(velocity.x, 0, move_data.friction)
 	
 	var was_in_air := not is_on_floor()
 	
---
+++ player_movement_data.gd
@@ -0,0 +1,8 @@
+class_name PlayerMovementData extends Resource
+
+
+@export var jump_velocity := -260.0
+@export var half_jump_velocity := -110.0
+@export var max_speed := 50.0
+@export var acceleration := 10.0
+@export var friction := 10.0
